29375e75a6e44f570576c6f75d6c700cbf0d9a60
Vladislav Khmelnitskii <khmelnitskii.vladislav@gmail.com>
2018-06-27 18:34:54 +0300

    [*] install.sh correct symlink

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -s .tm_dir-merge-rules $HOME/.tm_dir-merge-rules
+ln -s $(pwd)/.tm_dir-merge-rules $HOME/.tm_dir-merge-rules
 cp tm_rm_weekly /etc/cron.weekly/
 cp tm_rsync_root /etc/cron.hourly/
 cp tm_rsync_home /etc/cron.daily/
